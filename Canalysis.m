dc97567c5f7555f093510397764c2b4bcc1a508c
MAD1986 <dufour.martial@gmail.com>
2017-08-24 15:26:50 -0400

    fix bug

--- Canalysis.m
+++ Canalysis.m
@@ -1,7 +1,7 @@
 %%  MAD Ca2+ analysis
 
 %% Import data (XML, MAT, CSV)
-directory_name = '/Users/martial/Documents/Results/CA3_ThyGC6f/M6/D2' %folder with .mat .csv and .xml
+directory_name = '/Users/martial/Documents/Results/CA3_ThyGC6f/M4/D2' %folder with .mat .csv and .xml
 %!!!! files are listed by alphabetical order !!!!
 %!!!! Keep same name for mat and csv !!!!
 type='expdff';    %tye of calcium trace 'Cdf' ; 'expdff' or 'spikes' 
@@ -48,7 +48,7 @@ clear CSV XML options;
 %% Correct drifting baseline
 % Info 
 % https://www.mathworks.com/help/bioinfo/ref/msbackadj.html
-options.msbackadj=1; % 1 or 0 
+options.msbackadj=0; % 1 or 0 
 % Set parameters
 options.windwith=250; %width for the shifting window (in frame)
 options.dispfig=1; % Display figure 
@@ -69,7 +69,7 @@ clear options;
 options.restricted=1; %Event detection on restricted trace
 options.iterations=3; %Nb of iterations %Danielson et al. used 3 iterations
 options.SDOFF=0.5; %Threshold below x SD for OFFSET 
-options.msbackadj=1; % Corrected baseline
+options.msbackadj=0; % Corrected baseline
 % Test different threshold with histogram of error rate 
 % Lovett-Barron et al. 2014
 % Rajasethupathy et al. 2015
@@ -121,7 +121,7 @@ options.mindist= 10; % Set minimun distance between peaks (frame)
 options.STD_pro=2; % Set minimun prominence of peak ( X * STD noise)
 % Network parameters
 % synchronous epochs based on Rajasethupathy et al. 2015
-options.Nshuffle=1000; % Nb of shuffle for synchronous activity 
+options.Nshuffle=100; % Nb of shuffle for synchronous activity 
 options.pmin=0.05; % min p value to be considered as significant
 options.minframes=3; %consecutive frames with activity above the significance threshold
 
